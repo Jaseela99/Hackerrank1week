5b3bea505445cb2105329805438ab8f6bf9cb155
Jaseela99 <jaseelachittur@gmail.com>
2022-06-21 13:04:35 +0530

    added todays

--- week2.js
+++ week2.js
@@ -3,7 +3,7 @@ Caesar's cipher shifts each letter by a number of letters.
  If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet. 
  In the case of a rotation by 3, w, x, y and z would map to z, a, b and c. */
 
- let s = "There's-a-Car-outside."
+ /* let s = "There's-a-Car-outside."
  let k=3 
  function caesarCipher(s, k) {
     let alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -28,7 +28,7 @@ Caesar's cipher shifts each letter by a number of letters.
     return encrypted
 }
 console.log(caesarCipher(s,k))
-
+ */
 ////////////////////////////////////////////////////////////////////////
 //////////day2
 /* Given a square grid of characters in the range ascii[a-z], rearrange elements of each row alphabetically, ascending.
@@ -54,3 +54,23 @@ console.log(caesarCipher(s,k))
     return "YES";
 }
 console.log(gridChallenge(grid))
+///////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////day3
+/* We define super digit of an integer x  using the following rules:
+Given an integer, we need to find the super digit of the integer.
+If x has only 1 digit, then its super digit is x.
+Otherwise, the super digit of x is equal to the super digit of the sum of the digits ofx . */
+let n='8'
+let k=4
+function superDigit(n, k) {
+    //to number
+    if (n.length <= 1) return parseInt(n)
+    //to array---> sum is found --->sum initial=0 k-no of repitions
+    const sum= n.split('').reduce((sum, curr) => sum + parseInt(curr), 0) * k    
+    //conerted to string  
+    const sumStr= sum.toString()      
+    //if sum str is 1 sum is returned else recursion
+    return sumStr.length === 1 ? sum : superDigit(sumStr, 1)
+
+}
+console.log(superDigit(n,k))
