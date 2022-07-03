497828a990e07fba85d62f38aafe7a996664551d
Jaseela99 <jaseelachittur@gmail.com>
2022-07-03 15:32:20 +0530

    added todays

--- week3.js
+++ week3.js
@@ -90,3 +90,39 @@ function cookies(k, A) {
   return -1
 
 }
+//////////////////////////////////////////
+//NOprefix set
+function noPrefix(words) {
+    
+    let prefixes = new Set();   // All potential prefixes of words we've looked at so far
+    let wordsChecked = new Set();   // Words we've looked at so far
+    
+    for (const word of words) {
+        // See if the word is a prefix of any of the words we've checked so far.
+        if (prefixes.has(word)) {
+            console.log('BAD SET');
+            console.log(word);
+            return;
+        }
+    
+        // See if any version of the start of the word
+        // matches a word we've seen already
+        for (let wordLength = 1; wordLength <= word.length; ++wordLength) {
+            const prefixTarget = word.substr(0, wordLength);
+            if (wordsChecked.has(prefixTarget)) {
+                console.log('BAD SET');
+                console.log(word);
+                return;
+            }
+            
+            // Store as a potential prefix match for word we'll look at next
+            prefixes.add(prefixTarget);
+        }
+        // If we got this far, word becomes another prefix to try to match
+        wordsChecked.add(word);
+    }   // End loop through word list.
+
+    // If we got this far, we didn't find any prefixes
+    console.log('GOOD SET');
+
+}
